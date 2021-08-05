36c79ce7ccde0311d1d874f43078ac0a2e3d3313
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-08-05 12:26:21 -0700

    added animations and changed image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,24 +4,32 @@ import oceanPic from './ocean.jpeg';
 import './LandingPage.css'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import {Animated} from 'react-animated-css'
 
 
 
 
 const LandingPage = () => {
     return (
-        <Container >
-            <Row className="justify-content-md-center">
-                <Col id='col'>
-                     <img alt='logo' src={oceanPic}/>
-                     <p>ultra</p>
-                     <DropdownButton  variant="light" id="dropdown-basic-button" title="Enter">
-                        <Dropdown.Item href="#/action-1">Login</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Create Account</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-            </Row>
-        </Container>
+        <Animated 
+            animationIn="fadeIn" 
+            animationOut="fadeOut"
+            animationInDuration="3000"
+            isVisible={true}
+        >
+            <Container >
+                <Row className="justify-content-md-center">
+                    <Col id='col'>
+                        <img alt='logo' src={oceanPic}/>
+                        <p>ultra</p>
+                        <DropdownButton  variant="light" id="dropdown-basic-button" title="Enter">
+                            <Dropdown.Item href="#/action-1">Login</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Create Account</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
+                </Row>
+            </Container>
+        </Animated>
     );
 }
 
