fa70a29a03210bed30d2b68be8b0bfb68e985da6
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-08-04 17:28:51 -0700

    udpate

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 import LandingPage from '../components/LandingPage/LandingPage.js'
 
 class App extends Component {
