85867481226a8484211cf54024b440d344ad9291
alexchanocua <chanocuaalejandro@gmail.com>
2021-08-14 20:58:55 -0700

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import Navbar from 'react-bootstrap/Navbar'
+import {Container} from 'react-bootstrap'
+import './Footer.css'
+
+
+export default function Footer() {
+    return (
+        <div>
+                <Navbar id='footer' fixed='bottom'  bg="none" variant='light'>
+                    <Container>
+                        <Navbar.Collapse className="justify-content-center">
+                        <Navbar.Text >
+                            ultra 2021
+                        </Navbar.Text>
+                        </Navbar.Collapse>
+                    </Container>
+                 </Navbar>
+        </div>
+
+    )
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import homeImage3 from './homeImage3.jpg'
 import {Animated} from 'react-animated-css'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
+import Footer from '../Footer/Footer'
 import './Home.css'
 
 
@@ -42,83 +43,13 @@ export default function Home({name, onRouteChange}) {
                     </Animated>
                 </Col>
             </Row>
-            </Container>
-
-            {/* Testing scroll feature with duplicate images */}
-            <Animated 
-            animationIn="fadeIn" 
-            animationInDuration={2000}
-            animationInDelay={750} 
-            isVisible={true}
-            >
             <Row>
-            <p>{name}, thank you for registering. We are currently under development and will launch soon..</p>
+            <p>{name}, thank you for register. We are currently under development and will launch soon..</p>
             </Row>
-            </Animated>
-
-            <Container id='content' >
-            <Row className="justify-content-md-center">
-                <Col id='col'>
-                    <Animated 
-                        animationIn="fadeIn" 
-                        animationInDuration={3000}
-                        isVisible={true}
-                        >
-                    <img alt='logo' src={homeImage2}/>
-                    </Animated>
-                </Col>
-            </Row>
-
-            <Animated 
-            animationIn="fadeIn" 
-            animationInDuration={2000}
-            animationInDelay={750} 
-            isVisible={true}
-            >
-            <Row>
-            <p>This is a test layout of how everything will look.</p>
-            </Row>
-            </Animated>
             
-            </Container>
-
-            <Animated 
-            animationIn="fadeIn" 
-            animationInDuration={2000}
-            animationInDelay={750} 
-            isVisible={true}
-            >
-            <Row>
-            <p>{name}, thank you for registering. We are currently under development and will launch soon..</p>
-            </Row>
-            </Animated>
-
-            <Container id='content' >
-            <Row className="justify-content-md-center">
-                <Col id='col'>
-                    <Animated 
-                        animationIn="fadeIn" 
-                        animationInDuration={3000}
-                        isVisible={true}
-                        >
-                    <img alt='logo' src={homeImage}/>
-                    </Animated>
-                </Col>
-            </Row>
-
-            <Animated 
-            animationIn="fadeIn" 
-            animationInDuration={2000}
-            animationInDelay={750} 
-            isVisible={true}
-            >
-            <Row>
-            <p>This is a test layout of how everything will look.</p>
-            </Row>
-            </Animated>
             
+            <Footer></Footer>
             </Container>
-
         </div>
 
     )
