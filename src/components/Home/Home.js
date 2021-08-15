02e851691cfffc0b7405531fe9105ee4bb975d00
alexchanocua <chanocuaalejandro@gmail.com>
2021-08-14 17:41:32 -0700

    added another image to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import {Container, Row, Col} from 'react-bootstrap'
 import homeImage from './homeImage.jpg'
 import homeImage2 from './homeImage2.jpg'
+import homeImage3 from './homeImage3.jpg'
 import {Animated} from 'react-animated-css'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
@@ -37,13 +38,50 @@ export default function Home({name, onRouteChange}) {
                         animationInDuration={3000}
                         isVisible={true}
                         >
-                    <img alt='logo' src={homeImage2}/>
+                    <img alt='logo' src={homeImage3}/>
                     </Animated>
                 </Col>
             </Row>
             </Container>
 
             {/* Testing scroll feature with duplicate images */}
+            <Animated 
+            animationIn="fadeIn" 
+            animationInDuration={2000}
+            animationInDelay={750} 
+            isVisible={true}
+            >
+            <Row>
+            <p>{name}, thank you for registering. We are currently under development and will launch soon..</p>
+            </Row>
+            </Animated>
+
+            <Container id='content' >
+            <Row className="justify-content-md-center">
+                <Col id='col'>
+                    <Animated 
+                        animationIn="fadeIn" 
+                        animationInDuration={3000}
+                        isVisible={true}
+                        >
+                    <img alt='logo' src={homeImage2}/>
+                    </Animated>
+                </Col>
+            </Row>
+
+            <Animated 
+            animationIn="fadeIn" 
+            animationInDuration={2000}
+            animationInDelay={750} 
+            isVisible={true}
+            >
+            <Row>
+            <p>This is a test layout of how everything will look.</p>
+            </Row>
+            </Animated>
+            
+            </Container>
+
             <Animated 
             animationIn="fadeIn" 
             animationInDuration={2000}
@@ -79,7 +117,8 @@ export default function Home({name, onRouteChange}) {
             </Row>
             </Animated>
             
-        </Container>
+            </Container>
+
         </div>
 
     )
