8c34372ff69d7bc3d92417427209a5d3e7dbb56b
alexchanocua <chanocuaalejandro@gmail.com>
2021-08-20 19:36:28 -0700

    changed css - PreLandingPage

--- src/components/PreLandingPage/PreLandingPage.js
+++ src/components/PreLandingPage/PreLandingPage.js
@@ -7,8 +7,8 @@ export default function PreLandingPage ({onRouteChange}) {
     return (
         <div className="flex-container">
             <div className="flex-item">
-                <a onClick={() => onRouteChange('landingPage')}><img className='gif'  src={sphere} alt='loading'/></a>
-                <p className="gifText">ultra</p>
+                <a onClick={() => onRouteChange('landingPage')}><img className='gif'  src={sphere} alt='loading'/>
+                <p className="gifText">ultra</p></a>
             </div>
         </div>
                 
