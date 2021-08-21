e153c753ce334cbd0923f4703433ba5eefef64ad
alexchanocua <chanocuaalejandro@gmail.com>
2021-08-20 18:27:27 -0700

    centerd gif and text - PreLandingPage

--- src/components/PreLandingPage/PreLandingPage.js
+++ src/components/PreLandingPage/PreLandingPage.js
@@ -1,21 +1,17 @@
-import {Container, Col, Row} from 'react-bootstrap';
+
 import sphere from './sphere.gif'
 import './PreLandingPage.css'
 
 export default function PreLandingPage ({onRouteChange}) {
 
     return (
-        <Container className="container">
-            <Row>
-                <Col>
+        <div className="flex-container">
+            <div className="flex-item">
                 <a onClick={() => onRouteChange('landingPage')}><img className='gif'  src={sphere} alt='loading'/></a>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p className="gifText">ultra</p>
-                </Col>
-            </Row>
-        </Container>
+                <p className="gifText">ultra</p>
+            </div>
+        </div>
+                
+  
     )
 }
