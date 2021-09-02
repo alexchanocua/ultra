783bfa5427f4514c2caa5f9b455d24d9b9b35432
alexchanocua <chanocuaalejandro@gmail.com>
2021-09-02 13:59:54 -0700

    changed PreLanding page image

--- src/components/PreLandingPage/PreLandingPage.js
+++ src/components/PreLandingPage/PreLandingPage.js
@@ -1,5 +1,6 @@
 
 import sphere from './sphere.gif'
+import lock from './lock.jpg'
 import './PreLandingPage.css'
 
 export default function PreLandingPage ({onRouteChange}) {
@@ -7,7 +8,7 @@ export default function PreLandingPage ({onRouteChange}) {
     return (
         <div className="flex-container">
             <div className="flex-item">
-                <a onClick={() => onRouteChange('landingPage')}><img className='gif'  src={sphere} alt='loading'/>
+                <a onClick={() => onRouteChange('landingPage')}><img className='gif'  src={lock} alt='loading'/>
                 <p className="gifText">ultra</p></a>
             </div>
         </div>
